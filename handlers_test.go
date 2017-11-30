e2ec55655b48b8361991c73db710e3784937565f
Michael Crilly <mc@sent.com>
2017-11-30 19:09:23 +1000

    introducing more tests

--- handlers_test.go
+++ handlers_test.go
@@ -13,48 +13,73 @@ func init() {
 	ConfigFromString("")
 }
 
-type ExpectedResult struct {
+var HandlerBasicsTable = []struct {
+	Handler     httprouter.Handle
 	Verb        string
 	Path        string
 	Body        io.Reader
 	Response    *Message
 	ResultIsNil bool
-}
-
-func TestHandlerIndex(t *testing.T) {
-	expected := &ExpectedResult{
-		"GET",
-		"/",
-		nil,
-		&Message{
+}{
+	{
+		Handler: HandlerHealthCheck,
+		Verb:    "GET",
+		Path:    "/config",
+		Body:    nil,
+		Response: &Message{
+			Message: "alive",
+			Status:  200,
+		},
+		ResultIsNil: false,
+	},
+	{
+		Handler: HandlerReadOnlyConfig,
+		Verb:    "GET",
+		Path:    "/config",
+		Body:    nil,
+		Response: &Message{
 			Message: "ok",
 			Status:  200,
-			Result:  nil,
 		},
-		true,
-	}
+		ResultIsNil: false,
+	},
+	{
+		Handler: HandlerIndex,
+		Verb:    "GET",
+		Path:    "/",
+		Body:    nil,
+		Response: &Message{
+			Message: "ok",
+			Status:  200,
+		},
+		ResultIsNil: true,
+	},
+}
 
-	req, err := http.NewRequest(expected.Verb, expected.Path, expected.Body)
-	if err != nil {
-		t.Fatal(err)
-	}
+func TestHandlerBasics(t *testing.T) {
+	for _, tt := range HandlerBasicsTable {
+		req, err := http.NewRequest(tt.Verb, tt.Path, tt.Body)
+		if err != nil {
+			t.Fatal(err)
+		}
 
-	rr := httptest.NewRecorder()
-	handler := httprouter.New()
-	handler.GET(expected.Path, HandlerIndex)
-	handler.ServeHTTP(rr, req)
+		rr := httptest.NewRecorder()
+		handler := httprouter.New()
+		handler.GET(tt.Path, tt.Handler)
+		handler.ServeHTTP(rr, req)
 
-	result := MessageJsonToObject(rr.Body)
-	if result.Status != expected.Response.Status {
-		t.Errorf("Status code in response (%d) doesn't match what we expected (%d)",
-			result.Status, expected.Response.Status)
-	}
+		result := MessageJsonToObject(rr.Body)
+		if result.Status != tt.Response.Status {
+			t.Errorf("Status code in response (%d) doesn't match what we expected (%d)",
+				result.Status, tt.Response.Status)
+		}
 
-	if result.Message != expected.Response.Message {
-		t.Errorf("Message in response doesn't match what was expected: %s\n", result.Message)
-	}
+		if result.Message != tt.Response.Message {
+			t.Errorf("Message in response doesn't match what was expected: %s\n", result.Message)
+		}
 
-	if result.Result != nil && expected.ResultIsNil {
-		t.Error("Result should be nil for this endpoint")
+		if result.Result != nil && tt.ResultIsNil {
+			t.Error("Result should be nil for this endpoint")
+		}
 	}
 }
